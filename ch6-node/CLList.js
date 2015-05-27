e7bfe317743550a657b2fc2e88e4de1323c68963
Brian Capouch <brianc@proto.palaver.net>
2015-05-27 02:42:11 -0400

    Chapter 6 in the can

---
+++ ch6-node/CLList.js
@@ -0,0 +1,24 @@
+function Node(element) {
+  this.element = element;
+  this.next = null;
+}
+
+function CList() {
+  this.head = new Node("head");
+  this.head.next = this.head;
+  this.find = find;
+  this.insert = insert;
+  this.display = display;
+  this.findPrevious = findPrevious;
+  this.remove = remove;
+}
+
+function display() {
+  var currNode = this.head;
+  while (!(currNode.next === null) && !(currNode.next.element == "head")) {
+    console.log(currNode.next.element);
+    currNode = currNode.next;
+    }
+}
+
+module.exports.CList = CList;
