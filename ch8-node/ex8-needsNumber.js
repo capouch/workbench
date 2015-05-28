f4e1354e3955b71642b8912c9bc991edc1b1de27
Brian Capouch <brianc@proto.palaver.net>
2015-05-28 16:16:16 -0400

    Clean up comments and improve variable name

--- ch8-node/ex8-needsNumber.js
+++ ch8-node/ex8-needsNumber.js
@@ -14,47 +14,47 @@ var HashTable = require('./BetterHash2.js').HashTable;
 var pnumbers = new HashTable();
 var name, number;
 var cycle = 1;
-var inputMode = true;
+var inputPhase = true;
 
 // Node's ascync I/O vastly complicates this code!!
 rl.setPrompt("Enter a name ('finished' when done):  ");
 rl.prompt();
 rl.on('line', function(line) {
   // All done with both loops
-  if (inputMode == false && line === 'quit')
+  if (inputPhase == false && line === 'quit')
     rl.close();
   // All done with input, now do output loop
-  if (cycle == 1 && line === 'finished' && inputMode) {
-    inputMode = false;
+  if (cycle == 1 && line === 'finished' && inputPhase) {
+    inputPhase = false;
     name = "";
   } 
-  // Three possibilities here:
 
-  // First entry in input loop
-  if (cycle == 1 && inputMode )
+  // Three possibilities here:
+  // 1. First entry in input loop
+  if (cycle == 1 && inputPhase )
     name = line;
   else {
-    // Second entry in input loop
-    if (cycle == 0 && inputMode) {
+    // 2. Second entry in input loop
+    if (cycle == 0 && inputPhase) {
     number = line;
     pnumbers.put(name,number);
   } else {
-    // Output phase
+    // 3. Output phase
     name = line;
     console.log(name + "'s number is " + pnumbers.get(name));
     }
   } 
 
   // Set proper prompt 
-  if (cycle == 1 && inputMode)
+  if (cycle == 1 && inputPhase)
     rl.setPrompt('Enter a number: ');
-  else if (cycle == 0 && inputMode)
+  else if (cycle == 0 && inputPhase)
     rl.setPrompt("Enter a name ('finished' when done): ");
   else
     rl.setPrompt("Enter a name to look up ('quit' when done): ");
 
-  // Toggle cycle (on inputMode == true) and go again
-  cycle = (cycle == 1 && inputMode)?0:1;
+  // Toggle cycle (on inputPhase == true) and go again
+  cycle = (cycle == 1 && inputPhase)?0:1;
   rl.prompt();
 }).on('close',function(){
   process.exit(0);
