23af55e21a6c699b687b6d7e35bc3784dc430531
Brian Capouch <brianc@proto.palaver.net>
2015-06-01 03:12:03 -0400

    Twelve down, two to go!

---
+++ ch12-node/ex12-13.js
@@ -0,0 +1,8 @@
+var mergeSort = require('./mergeSortStart.js').mergeSort;
+
+var nums = [6,10,1,9,4,8,2,7,3,5];
+console.log(nums);
+console.log();
+mergeSort(nums);
+console.log();
+console.log(nums);
---
+++ ch12/ex12-13-2.js
@@ -0,0 +1,7 @@
+load ('./CArray.js');
+
+var nums = new CArray(10);
+nums.setData();
+print(nums.toString());
+nums.mergeSort();
+print(nums.toString());
